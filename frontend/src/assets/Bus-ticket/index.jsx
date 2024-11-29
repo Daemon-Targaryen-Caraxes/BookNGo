9ffecba019e61a703fab70c5e3eb48f5e0601cca
Venkat14424 <lalivenkatasai2736@gmail.com>
2024-11-29 16:01:53 +0530

    add the bus personal details

--- frontend/src/assets/Bus-ticket/index.jsx
+++ frontend/src/assets/Bus-ticket/index.jsx
@@ -2,8 +2,27 @@
 function bus (){
  return(
   <div className="busTicket">
-  <h2>hello</h2>
+
+  <div className="pinfo">
+  <h2>Bus Ticket Reservation System</h2>
+  <h3>Passenger Information</h3>
+  <p><b>Name : </b><input type="text" placeholder="Full Name" required></input></p>
+  <p><b>Age : </b> <input type="text" placeholder="Your Age" required></input></p>
+  <p>  <b>Gender :</b><br></br>
+    <input type="radio" id="Male" name="Gender" Value></input>
+    <label for="Male" >Male</label><br></br>
+    <input type="radio" id="Female" name="Gender" Value></input>
+    <label for="Female" >Female</label><br></br>
+    <input type="radio" id="Other" name="Gender" Value></input>
+    <label for="Other" >Other</label><br></br>
+  </p>
+  <p><b>Contact : </b><input type="number" placeholder="Yourcontact" required></input></p>
+  <p><b>Email : </b><input type="email" placeholder="Example@gmail.com" required></input></p>
+
   </div>
+  <button class="submit" type="button" required>Submit</button>
+  </div>
+  
  )
 }
 export default bus;
