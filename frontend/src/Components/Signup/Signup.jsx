c221841537931399b0c740e5f13a935304463c53
Hemeswar reddy <157202138+Daemon-Targaryen-Caraxes@users.noreply.github.com>
2024-12-08 18:55:53 +0530

    adds some styles to signup page

--- frontend/src/Components/Signup/Signup.jsx
+++ frontend/src/Components/Signup/Signup.jsx
@@ -3,34 +3,48 @@ import './SignupStyles.css'
 
 const Signup = () => {
   return (
-    <div className="container">
+    <div className="form-container">
       <h1 className="h1">Signup With Your Details </h1>
-      <form className="signup" action="">
-        <label htmlFor="Name:">Name:</label>
-        <input className="name" type="text" id="Name" placeholder="Name" />
-        <label htmlFor="Phone No">Phone No:</label>
-        <input type="text" id="Phone No" placeholder="" />
-        <label htmlFor="Email:">Email:</label>
-        <input className="email" type="text" id="Email" placeholder="Ex:project@gmail.com" />
-        Your Gender:
-        <label htmlFor="male">Male</label>
-        <input type="radio" id="Male" value="Male" name='gender' />
-        <label htmlFor="female">Female</label>
-        <input type="radio" id='Female' value="Female" name='gender' />
-        <label htmlFor="others">Others</label>
-        <input type="radio" id='others' value="others" name='gender' />
-        <label htmlFor="Aadhar:">Aadhar No:</label>
-        <input type="Number" id="Aadhar No" placeholder="**** **** ****" />
-        <label htmlFor="Email:">User ID:</label>
-        <input className="userid" type="text" id="User ID" placeholder="*" />
-        <label htmlFor="Email:">Password:</label>
-        <input className="password" type="Number" id="Password" placeholder="" />
-        <label htmlFor="Email:">Confirm <br />Password:</label>
-        <input className="password" type="Number" id="Password" placeholder="*" />
-        <button className="button">Signin</button>
-        <p className="p1">Already have an account? Log in</p>
-        <label htmlFor=""></label>
-        <input className="signup-with-google" id="password" type="Number" placeholder="Signup with Google" />
+      <form className="signup" action="/">
+        <div>
+          <label htmlFor="Name:">Name:</label>
+          <input className="name" type="text" id="Name" placeholder="Name" />
+        </div>
+        <div>
+          <label htmlFor="Phone No">Phone No:</label>
+          <input type="text" id="Phone No" placeholder="" />
+        </div>
+        <div>
+          <label htmlFor="Email:">Email:</label>
+          <input className="email" type="text" id="Email" placeholder="Ex:project@gmail.com" />
+        </div>
+        <div>
+          Gender:
+          <input type="radio" id="Male" value="Male" name='gender' />
+          <label htmlFor="male"> Male</label>
+          <input type="radio" id='Female' value="Female" name='gender' />
+          <label htmlFor="female">Female</label>
+          <input type="radio" id='others' value="others" name='gender' />
+          <label htmlFor="others">Others</label>
+        </div>
+        <div>
+          <label htmlFor="Aadhar:">Aadhar No:</label>
+          <input type="Number" id="Aadhar No" placeholder="**** **** ****" />
+        </div>
+        <div>
+          <label htmlFor="Email:">User ID:</label>
+          <input className="userid" type="text" id="User ID" placeholder="*" />
+        </div>
+        <div>
+          <label htmlFor="Email:">Password:</label>
+          <input className="password" type="Number" id="Password" placeholder="" />
+        </div>
+        <div>
+          <label htmlFor="Email:">Confrm <br />Password:</label>
+          <input className="password" type="Number" id="Password" placeholder="*" />
+        </div>
+        <button className="signup-button">Signup</button>
+        <p className="p1">Already have an account? Login</p>
       </form>
     </div>
   )
