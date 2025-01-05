7795a0d16158bcb03a8c12ffe96d622122fb9cc6
Hemeswar reddy <hemeswarreddy74@gmail.com>
2025-01-05 11:41:03 +0530

    adds some styles to admin login page

--- frontend/src/components/adminside/Adminlogin.jsx
+++ frontend/src/components/adminside/Adminlogin.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import "./AdminLoginStyles.css"
+
 function AdminLogin() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -6,10 +8,10 @@ function AdminLogin() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!username || !password) {
-      setError('Username and password are required!');
-      return;
-    }
+    // if (!username || !password) {
+    //   setError('Username and password are required!');
+    //   return;
+    // }
     if (username === 'admin' && password === 'admin123') {
       setError('');
       alert('Login successful');
@@ -17,17 +19,18 @@ function AdminLogin() {
       setError('Invalid username or password');
     }
   };
+
   return (
     <div className="login-container">
-      <h2>Admin Login</h2>
+      <h2 className='admin-login'>Admin Login</h2>
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter username" />
+          <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter username" required />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password" />
+          <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password" required />
         </div>
         {error && <p className="error">{error}</p>}
         <button type="submit">Login</button>
