c089719aac19d4d3691b4c54bd8c5808c40811b8
Venkat14424 <lalivenkatasai2736@gmail.com>
2025-01-05 12:17:58 +0530

    change the styles

--- frontend/src/components/userside/Usersignup.jsx
+++ frontend/src/components/userside/Usersignup.jsx
@@ -41,6 +41,7 @@ const SignUp = () => {
       return;
     }
     alert("Sign-Up Successful!");
+    navigate("/TravelSearchForm")
     console.log("User Data:", formData);
     setFormData({
       username: "",
@@ -60,20 +61,20 @@ const SignUp = () => {
       <h2>Sign Up Page</h2>
       {error && <p className="error">{error}</p>}
       <form onSubmit={handleSubmit}>
-        <input type="text" name="username" placeholder="Username"  value={formData.username} onChange={handleChange}/>
-        <input type="number" name="phone"   placeholder="Phone Number" value={formData.phone}  onChange={handleChange} />
-        <select name="gender" value={formData.gender} onChange={handleChange}>
+        <input type="text" name="username" placeholder="Username"  value={formData.username} onChange={handleChange} required/>
+        <input type="number" name="phone"   placeholder="Phone Number" value={formData.phone}  onChange={handleChange} required />
+        <select name="gender" value={formData.gender} onChange={handleChange} required>
           <option value="">Select Gender</option>
           <option value="Male">Male</option>
           <option value="Female">Female</option>
           <option value="Other">Other</option>
         </select>
-        <input type="date"  name="dob" placeholder="Date of Birth" value={formData.dob} onChange={handleChange} />
-        <input type="text" name="aadhaar" placeholder="Aadhaar Number"  value={formData.aadhaar}  onChange={handleChange}/>
-        <input type="text" placeholder="UserId"  value={formData.userid} onChange={handleChange}/>
-        <input type="password" name="password"  placeholder="Password"  value={formData.password} onChange={handleChange} />
-        <input  type="password"  name="confirmPassword"  placeholder="Confirm Password" value={formData.confirmPassword} onChange={handleChange} />
-        <button type="submit" onClick={()=>navigate("/TravelSearchForm")}>Sign Up</button>
+        <input type="date"  name="dob" placeholder="Date of Birth" value={formData.dob} onChange={handleChange} required />
+        <input type="text" name="aadhaar" placeholder="Aadhaar Number"  value={formData.aadhaar}  onChange={handleChange} required/>
+        <input type="text" name="userid" placeholder="UserId"  value={formData.userid} onChange={handleChange} required/>
+        <input type="password" name="password"  placeholder="Password"  value={formData.password} onChange={handleChange}  required/>
+        <input  type="password"  name="confirmPassword"  placeholder="Confirm Password" value={formData.confirmPassword} onChange={handleChange} required />
+        <button type="submit">Sign Up</button>
       </form>
     </div>
     </>
