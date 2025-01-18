50ce16698d6a8eb2faff21ce9be369e26494b8af
Daemon Targaryen <hemeswarreddy74@gmail.com>
2025-01-18 16:37:22 +0530

    created a component called passenger details

---
+++ frontend/src/Components/adminside/PassengerDetails.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PassengerDetails = () => {
+  return (
+    <div>
+      <div>Passenger Name: </div>
+      <div>Gender: </div>
+      <div>Phone Number: </div>
+      <div>Date of Birth: </div>
+      <div>Aadhar No: </div>
+    </div>
+  )
+}
+
+export default PassengerDetails
