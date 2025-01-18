5a22ac3b04658d5fd81a4e5c9f7dc0471dba0e86
Daemon Targaryen <hemeswarreddy74@gmail.com>
2025-01-18 17:13:02 +0530

    adds data in the document

--- frontend/src/Components/adminside/PassengerDetails.jsx
+++ frontend/src/Components/adminside/PassengerDetails.jsx
@@ -1,25 +1,33 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 
 const PassengerDetails = () => {
+  const [fetchedData, setFetchedData] = useState({});
   const location = useLocation();
+  console.log(fetchedData);
   
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(`http://localhost:3000/user/${location.pathname.slice(1)}`);
       const data = await response.json();
-      console.log(data);
+      setFetchedData(data);
     }
     fetchData();
-  }, [])
+  }, []);
+
+  const { username, phone, gender, dob, userid, aadhaar } = fetchedData;
+  // console.log(username, phone, gender, dob, userid, aadhaar);
+  console.log(phone.slice(0,10));
 
   return (
-    <div className='text-white'>
-      <div>Passenger Name: </div>
-      <div>Gender: </div>
-      <div>Phone Number: </div>
-      <div>Date of Birth: </div>
-      <div>Aadhar No: </div>
+    <div className='m-auto my-24 w-96 p-5 text-white'>
+      <h1 className='text-center text-3xl'>Passenger Details</h1>
+      <div>Passenger Name: {username}</div>
+      <div>Passenger ID: {userid}</div>
+      <div>Gender: {gender}</div>
+      <div>Phone Number: {phone}</div>
+      <div>Date of Birth: {dob && dob.slice(0, 10)}</div>
+      <div>Aadhar No: {aadhaar}</div>
     </div>
   )
 }
--- frontend/src/Components/adminside/PassengerEnquiry.jsx
+++ frontend/src/Components/adminside/PassengerEnquiry.jsx
@@ -18,7 +18,7 @@ const PassengerEnquiry = () => {
 
   return (
     <div className='text-center m-auto my-24 w-96 p-5'>
-      <h1 className='text-5xl text-white'>Passenger Enquiry</h1>
+      <h1 className='text-3xl text-white'>Passenger Enquiry</h1>
       <div className='text-red-500'>{error}</div>
       <div>
         <label htmlFor="phone-number" className='mr-1 text-lg float-left block'>Phone No.</label>
